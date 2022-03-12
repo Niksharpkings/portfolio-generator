17a481c1cac8ce0d0d587f331e890010af9e7d76
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-03-12 14:54:02 -0800

    node inquire

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message:'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+
+
+
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
 
 
